347ea40aabf83a5a83493ae2cb6077a65bebea1c
ianyimstudent <ianyim@arizona.edu>
2025-07-17 21:43:56 +0900

    created a branch and refactored doMove with meaningful naming

--- src/move.c
+++ src/move.c
@@ -5,25 +5,25 @@ void doMove(chess_board *board, move *move) {
     board->board[IDX_metadataB] = (board->board[IDX_metadataB] & ~MASK_EN_PASSANT_AND_CASTLE) | (move->metadataM & MASK_EN_PASSANT_AND_CASTLE);
     
     if(move->metadataM & DO_WK_CASTLE) {
-        board->board[IDX_K] = (board->board[IDX_K] & ~0x8ULL) | 0x2ULL;
-        board->board[IDX_R] = (board->board[IDX_R] & ~0x1ULL) | 0x4ULL;
-        board->board[IDX_empty] = (board->board[IDX_empty] & ~0x6ULL) | 0x9ULL;
-        board->board[IDX_white] = (board->board[IDX_white] & ~0x9ULL) | 0x6ULL;
+        board->board[IDX_K] = (board->board[IDX_K] & ~e1) | g1;
+        board->board[IDX_R] = (board->board[IDX_R] & ~h1) | f1;
+        board->board[IDX_empty] = (board->board[IDX_empty] & ~(g1 | f1)) | (e1 | h1);
+        board->board[IDX_white] = (board->board[IDX_white] & ~(e1 | h1)) | (g1 | f1);
     } else if(move->metadataM & DO_WQ_CASTLE) {
-        board->board[IDX_K] = (board->board[IDX_K] & ~0x8ULL) | 0x20ULL;
-        board->board[IDX_R] = (board->board[IDX_R] & ~0x80ULL) | 0x10ULL;
-        board->board[IDX_empty] = (board->board[IDX_empty] & ~0x30ULL) | 0x81ULL;
-        board->board[IDX_white] = (board->board[IDX_white] & ~0x81ULL) | 0x30ULL;
+        board->board[IDX_K] = (board->board[IDX_K] & ~e1) | c1;
+        board->board[IDX_R] = (board->board[IDX_R] & ~a1) | d1;
+        board->board[IDX_empty] = (board->board[IDX_empty] & ~(c1 | d1)) | (e1 | a1);
+        board->board[IDX_white] = (board->board[IDX_white] & ~(e1 | a1)) | (c1 | d1);
     } else if(move->metadataM & DO_BK_CASTLE) {
-        board->board[IDX_k] = (board->board[IDX_k] & ~0x0800000000000000ULL) | 0x0200000000000000ULL;
-        board->board[IDX_r] = (board->board[IDX_r] & ~0x0100000000000000ULL) | 0x0400000000000000ULL;
-        board->board[IDX_empty] = (board->board[IDX_empty] & ~0x0600000000000000ULL) | 0x0900000000000000ULL;
-        board->board[IDX_black] = (board->board[IDX_black] & ~0x0900000000000000ULL) | 0x0600000000000000ULL;
+        board->board[IDX_k] = (board->board[IDX_k] & ~e8) | g8;
+        board->board[IDX_r] = (board->board[IDX_r] & ~h8) | f8;
+        board->board[IDX_empty] = (board->board[IDX_empty] & ~(g8 | f8)) | (e8 | h8);
+        board->board[IDX_black] = (board->board[IDX_black] & ~(e8 | h8)) | (g8 | f8);
     } else if(move->metadataM & DO_BQ_CASTLE) {
-        board->board[IDX_k] = (board->board[IDX_k] & ~0x0800000000000000ULL) | 0x2000000000000000ULL;
-        board->board[IDX_r] = (board->board[IDX_r] & ~0x8000000000000000ULL) | 0x1000000000000000ULL;
-        board->board[IDX_empty] = (board->board[IDX_empty] & ~0x3000000000000000ULL) | 0x8100000000000000ULL;
-        board->board[IDX_black] = (board->board[IDX_black] & ~0x8100000000000000ULL) | 0x3000000000000000ULL;
+        board->board[IDX_k] = (board->board[IDX_k] & ~e8) | c8;
+        board->board[IDX_r] = (board->board[IDX_r] & ~a8) | d8;
+        board->board[IDX_empty] = (board->board[IDX_empty] & ~(c8 | d8)) | (e8 | a8);
+        board->board[IDX_black] = (board->board[IDX_black] & ~(e8 | a8)) | (c8 | d8);
     } else if(move->metadataM & DO_EN_PASSANT) {
         if(board->board[IDX_metadataB] & FLAG_W_TURN) {
             board->board[IDX_empty] = (board->board[IDX_empty] & ~(move->to)) | move->from | (move->to >> 8); 
